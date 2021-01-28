1dd9eac8e1d4a9a7f8d873f9eb7fb8b7922255a3
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-28 17:13:48 -0300

    testando o rest e criando método listar

--- src/main/java/com/viniciusalcantaracursomc/resources/CategoriaResource.java
+++ src/main/java/com/viniciusalcantaracursomc/resources/CategoriaResource.java
@@ -1,10 +1,15 @@
 package com.viniciusalcantaracursomc.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando";
+	}
 	
 }
