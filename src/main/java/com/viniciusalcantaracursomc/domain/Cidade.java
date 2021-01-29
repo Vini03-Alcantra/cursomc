5599ccb5189cb3a55522e1167a37d03be1589560
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-29 18:52:53 -0300

    implementnando classes estado e cidade

--- src/main/java/com/viniciusalcantaracursomc/domain/Cidade.java
+++ src/main/java/com/viniciusalcantaracursomc/domain/Cidade.java
@@ -1,13 +1,24 @@
 package com.viniciusalcantaracursomc.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cidade {
+public class Cidade implements Serializable{	
+	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String nome;
 	
 	private Estado estado;
+	
+	public Cidade() {}
+	
+	public Cidade(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
 
 	public Integer getId() {
 		return id;
@@ -31,6 +42,31 @@ public class Cidade {
 
 	public void setEstado(Estado estado) {
 		this.estado = estado;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
