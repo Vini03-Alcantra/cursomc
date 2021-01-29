62bdb8e93c93531446a0bd7066657205db5fa08c
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-28 23:47:57 -0300

    Criando endpoint de categorias

---
+++ src/main/java/com/viniciusalcantaracursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.viniciusalcantaracursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.viniciusalcantaracursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+}
---
+++ src/main/java/com/viniciusalcantaracursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.viniciusalcantaracursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciusalcantaracursomc.domain.Categoria;
+import com.viniciusalcantaracursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);		
+	}
+}
