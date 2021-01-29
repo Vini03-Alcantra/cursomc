8b185669430d616c495230a71823d95a5459f509
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-29 17:34:52 -0300

    Obtendo resposta quando procurar por id não existente

--- src/main/java/com/viniciusalcantaracursomc/resources/exception/StandardError.java
+++ src/main/java/com/viniciusalcantaracursomc/resources/exception/StandardError.java
@@ -1,6 +1,10 @@
 package com.viniciusalcantaracursomc.resources.exception;
 
-public class StandardError {
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
 	private String msg;
