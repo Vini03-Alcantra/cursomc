02e533b012853e2e804744527489b3fe58a1da08
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-29 20:09:29 -0300

    CRiando classes cliente e endereco

---
+++ src/main/java/com/viniciusalcantaracursomc/domain/Cliente.java
@@ -0,0 +1,11 @@
+package com.viniciusalcantaracursomc.domain;
+
+import com.viniciusalcantaracursomc.domain.enums.TipoCliente;
+
+public class Cliente {
+	private Integer id; 
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private TipoCliente tipo;
+}
---
+++ src/main/java/com/viniciusalcantaracursomc/domain/Endereco.java
@@ -0,0 +1,10 @@
+package com.viniciusalcantaracursomc.domain;
+
+public class Endereco {
+	private Integer id;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cep;
+}
