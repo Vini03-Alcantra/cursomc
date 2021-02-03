66f820221fe57d17e5d3bd002a90acac4afe7a44
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-02 22:47:34 -0300

    Criando classe pagamentoComBoleto

---
+++ src/main/java/com/viniciusalcantaracursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,31 @@
+package com.viniciusalcantaracursomc.domain;
+
+import java.util.Date;
+
+public class PagamentoComBoleto {
+	private Date dataVencimento;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {}
+	
+	public PagamentoComBoleto(Date dataVencimento, Date dataPagamento) {
+		super();
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+	}
+	
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+}
