2e213d109969e64c95e4b1561682b4b34b324fb4
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-02 22:31:17 -0300

    Crianco classe pedido

---
+++ src/main/java/com/viniciusalcantaracursomc/domain/Pedido.java
@@ -0,0 +1,24 @@
+package com.viniciusalcantaracursomc.domain;
+
+import java.util.Date;
+
+public class Pedido {
+	private Integer id;
+	private Date instante;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getInstante() {
+		return instante;
+	}
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+	
+	
+}
