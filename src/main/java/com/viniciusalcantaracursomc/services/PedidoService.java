92a380edf2ccc5fd76919c65d23b91649fd2911a
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-04 15:53:10 -0300

    Criando pedidoservice

---
+++ src/main/java/com/viniciusalcantaracursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.viniciusalcantaracursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciusalcantaracursomc.domain.Cliente;
+import com.viniciusalcantaracursomc.domain.Pedido;
+import com.viniciusalcantaracursomc.repositories.PedidoRepository;
+import com.viniciusalcantaracursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado Id:" +id+ "Tipo:" + Cliente.class.getName()
+		));
+	}
+}
+
