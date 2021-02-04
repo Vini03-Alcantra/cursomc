148ecb6c96baf0fabb14fdae34d1169d232e9263
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-04 16:19:23 -0300

    criando endpoint e disponibilizando /pedidos/{id}

--- src/main/java/com/viniciusalcantaracursomc/domain/ItemPedido.java
+++ src/main/java/com/viniciusalcantaracursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -36,11 +39,12 @@ public class ItemPedido implements Serializable{
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
-
+	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+		
 	public Produto getProduto() {
 		return id.getProduto();
 	}
