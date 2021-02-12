50a6d8040e63c747dfd6c67a06f2a8e769282a33
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-12 18:34:12 -0300

    Vindo apenas nome e id das categorias"

---
+++ src/main/java/com/viniciusalcantaracursomc/dto/CategoriaDTO.java
@@ -0,0 +1,37 @@
+package com.viniciusalcantaracursomc.dto;
+
+import java.io.Serializable;
+
+import com.viniciusalcantaracursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersion = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {}
+
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
--- src/main/java/com/viniciusalcantaracursomc/resources/CategoriaResource.java
+++ src/main/java/com/viniciusalcantaracursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.viniciusalcantaracursomc.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.viniciusalcantaracursomc.domain.Categoria;
+import com.viniciusalcantaracursomc.dto.CategoriaDTO;
 import com.viniciusalcantaracursomc.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -51,8 +53,11 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<Categoria>> findAll(){
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
 }
