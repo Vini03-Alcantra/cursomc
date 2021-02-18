e771851806f488554d68c8590b06f2a064da36cd
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-18 18:31:31 -0300

    Atualizando cliente

--- src/main/java/com/viniciusalcantaracursomc/dto/ClienteDTO.java
+++ src/main/java/com/viniciusalcantaracursomc/dto/ClienteDTO.java
@@ -2,13 +2,24 @@ package com.viniciusalcantaracursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.viniciusalcantaracursomc.domain.Cliente;
 
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 a 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Mensagem Inválida")
 	private String email;
 	
 	public ClienteDTO() {}
--- src/main/java/com/viniciusalcantaracursomc/services/ClienteService.java
+++ src/main/java/com/viniciusalcantaracursomc/services/ClienteService.java
@@ -29,8 +29,9 @@ public class ClienteService {
 	}
 	
 	public Cliente update(Cliente obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -52,8 +53,11 @@ public class ClienteService {
 	}
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
-		throw new UnsupportedOperationException();
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 	
-	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
