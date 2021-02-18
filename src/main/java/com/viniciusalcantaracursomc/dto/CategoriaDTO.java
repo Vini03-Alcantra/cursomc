4fca0602c6e2b956cfed3abb0a58a830ba263d2c
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-18 16:13:56 -0300

    Validação sintática usando o BeanValidation

--- src/main/java/com/viniciusalcantaracursomc/dto/CategoriaDTO.java
+++ src/main/java/com/viniciusalcantaracursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.viniciusalcantaracursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.viniciusalcantaracursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersion = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 a 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
