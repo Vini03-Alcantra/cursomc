030071a2a24f11fe4c1223494a19cddea24f8141
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-18 18:02:01 -0300

    Ciente DTO

--- src/main/java/com/viniciusalcantaracursomc/services/ClienteService.java
+++ src/main/java/com/viniciusalcantaracursomc/services/ClienteService.java
@@ -1,13 +1,19 @@
 package com.viniciusalcantaracursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-
 import com.viniciusalcantaracursomc.domain.Cliente;
+import com.viniciusalcantaracursomc.dto.ClienteDTO;
 import com.viniciusalcantaracursomc.repositories.ClienteRepository;
+import com.viniciusalcantaracursomc.services.exceptions.DataIntegrityException;
 import com.viniciusalcantaracursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,7 +25,35 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado Id:" +id+ "Tipo:" + Cliente.class.getName()
-		));
-		
+		));	
 	}
+	
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		throw new UnsupportedOperationException();
+	}
+	
+	
 }
