784e9e9994029af39405aa86cb91124b07fd96bd
Vinicus <viniciusalcantarabarros@gmail.com>
2021-03-04 00:53:19 -0300

    Email repetido na atualização de cliente

--- src/main/java/com/viniciusalcantaracursomc/dto/ClienteDTO.java
+++ src/main/java/com/viniciusalcantaracursomc/dto/ClienteDTO.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.viniciusalcantaracursomc.domain.Cliente;
-
+import com.viniciusalcantaracursomc.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
