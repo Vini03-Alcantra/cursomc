30e7d5f89b7014657f2868ceb4629d073376971f
Vinicus <viniciusalcantarabarros@gmail.com>
2021-03-05 14:53:23 -0300

    JacksonConfig

--- src/main/java/com/viniciusalcantaracursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/viniciusalcantaracursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.viniciusalcantaracursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.viniciusalcantaracursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
